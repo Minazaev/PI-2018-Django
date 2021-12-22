0d996fda1acf9c03bbbbc83c65c6b5462303827f
Bolide9 <dgabrailov2000@mail.ru>
2021-12-22 13:33:57 +0300

    'fix'

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,10 @@
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to view_set. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function view_set
-    1. Add an import:  from my_app import view_set
-    2. Add a URL to urlpatterns:  path('', view_set.home, name='home')
-Class-based view_set
-    1. Add an import:  from other_app.view_set import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
 from contacts.view_set.contact_view_set import ContactViewSet
 from friends.view_set.friend_view_set import FriendViewSet
 from rest_framework import serializers, viewsets
-from router.router import register_route, router
+from api.router import register_route, router
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -42,6 +27,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path(r'contact/<int:id>/', ContactViewSet.as_view({'get': 'retrieve'}), name='contact-detail')
 ]
 
