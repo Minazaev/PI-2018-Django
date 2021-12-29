c50917a6505e8ad0e89ac6a4d32d388293a6fb44
Bolide9 <dgabrailov2000@mail.ru>
2021-12-29 13:30:11 +0300

    ref

---
+++ addresses/serializers/address_serializer.py
@@ -0,0 +1,10 @@
+from addresses.models.address import Address
+from rest_framework import serializers
+
+
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Address
+        fields = '__all__'
+
