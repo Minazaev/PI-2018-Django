4edf9224b50ed8d84787d73e66f8cd5fa04d755d
Bolide9 <dgabrailov2000@mail.ru>
2022-03-30 13:46:15 +0300

    fix user admin

--- users/models.py
+++ users/models.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class CustomUser(AbstractUser):
-    objects = models.Manager()
+    objects = UserManager()
 
     def __str__(self):
         return self.get_full_name()
 
-    def __unicode__(self):
-        return self.get_full_name()
+    # def __unicode__(self):
+    #     return self.get_full_name()
